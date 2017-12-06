6b8ebccf5c765d4160d40cdeaa819e87ccbdb918
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-12-06 16:26:41 -0500

    resize

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
+
+
 $(document).ready(function(){
     $("#apc").hide()
 
     $("i").click(function(){
     	$("#apc").fadeToggle()
     })
+
+    currentHeight= $("#height").height()
+    console.log(currentHeight)
+    
+    $(window).resize(function(){
+    	$("#atlanta").css({"height": $("#height").height()});
+    })
+    
+		
+
 });
 
