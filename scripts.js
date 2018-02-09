78149a288151f2924e3a9c757f6c38ea601a8082
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-02-09 08:48:43 -0500

    project section, nav bar

--- scripts.js
+++ scripts.js
@@ -22,7 +22,58 @@ $(document).ready(function(){
     // $('.project-image').mouseout(function () {
     //       $('.project-image').show();      
     // });
-		
+    $(".show-me").hide()
+    
+    $("#react").hover(function(){
+        $("#show-react").fadeToggle()
+    })
+
+    $("#node").hover(function(){
+        $("#show-node").fadeToggle()
+    })
+
+    $("#html").hover(function(){
+        $("#show-html").fadeToggle()
+    })
+
+    $("#css").hover(function(){
+        $("#show-css").fadeToggle()
+    })
+
+    $("#py").hover(function(){
+        $("#show-py").fadeToggle()
+    })
+    $("#py").hover(function(){
+        $("#show-rpg").fadeToggle()
+    })
+
+    $("#redux").hover(function(){
+        $("#show-redux").fadeToggle()
+    })
+
+    $("#sql").hover(function(){
+        $("#show-sql").fadeToggle()
+    })
+	
+    $("#git").hover(function(){
+        $("#show-git").fadeToggle()
+    })
+
+    $("#js").hover(function(){
+        $("#show-js").fadeToggle()
+    })
+
+    $("#jquery").hover(function(){
+        $("#show-jquery").fadeToggle()
+    })
+
+    $("#aws").hover(function(){
+        $("#show-aws").fadeToggle()
+    })
+
+    $("#express").hover(function(){
+        $("#show-express").fadeToggle()
+    })
 
 });
 
